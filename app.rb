27dba31a8dd049abf94c5c474a14d90c4f5e7a7a
Miguel Peñaloza <micky.ale7@gmail.com>
2020-06-15 17:56:36 -0400

    Dar formato a index.erb y a app.rb
    Simplemente se corrigio los formatos, identaciones

--- app.rb
+++ app.rb
@@ -2,22 +2,21 @@ require 'sinatra'
 require './lib/TorosVacas.rb'
 require './lib/cronometro.rb'
 
-def initialize()
-    # $torovaca=ToroVaca.new("")
-    # $contador=11
-end
 get '/' do    
     $torovaca=ToroVaca.new("")
     $contador=11
     erb :index
 end
+
 post '/juego' do
     $contador -= -1
     erb :juego
 end
+
 post '/intento' do
     erb :intento
 end
+
 get '/configuracion' do
     erb :configuracion
 end
